72da25fcfe8483d206b95dbc3a5577069e001cf8
Shannonwje <e0325213@u.nus.edu>
2020-05-04 16:38:41 +0800

    Updated Order & OrderDetails schema & Added queries for Customers

--- Database/query.sql
+++ Database/query.sql
@@ -7,37 +7,10 @@ INSERT INTO PartTimers (riderId) VALUES ($1); --add part timers
 INSERT INTO FullTimers (riderId) VALUES ($1); --add full timers
 DELETE FROM Customers where customerId = $1; --delete users
 
-INSERT INTO Orders (orderId, CustomerId, riderId, restaurantId, dateOfOrder, timeOfOrder, deliveryLocationArea, totalCost, departureTimeToRestaurant, arrivialTimeAtRestaurant, departureTimeToDestination, arrivalTimeAtDestination) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
+INSERT INTO Orders (orderId, CustomerId, riderId, restaurantId, dateOfOrder, timeOfOrder, deliveryLocationArea, totalCost, departureTimeToRestaurant, arrivialTimeAtRestaurant, departureTimeToDestiantion, arrivalTimeAtDestiantion) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
 
 /*Restaurant staff Functions*/
-SELECT itemName
-FROM Menus
-WHERE --see all menu items
---filter by cuisine
---set menus utems daily limit
---update menu item information
---select promo
-
-/*Customer Functions*/
---select restaurant from list
---see all menu items
---add menu items to order
---filter by cuisine
---select price range
---select payment method
---enter card number (if payment by card)
---select recent order
---select promo
---See review posting
-SELECT DISTINCT Res.name as Restaurant, O.dateOfOrder as OrderDate, Rev.review as Review, Rev.rating as Rating
-FROM Restaurant Res JOIN Orders O USING (restaurantId) JOIN Reviews Rev USING (orderId)
-ORDER BY Res.name
-;
--- See past orders
-SELECT as OrderDate, as Restaurant, as ItemName, as Quantity
-FROM Orders O JOIN 
-;
--- See resturant info
+-- See restaurant info
 SELECT DISTINCT R.restaurantId, name, area, minSpendingAmt
 FROM Restaurants INNER JOIN RestaurantStaff RS on R.restaurantId =  RS.restaurantId
 WHERE RS.restaurantId = $1 LIMIT 1
@@ -62,28 +35,28 @@ UPDATE Menus
 SET price = $1
 WHERE itemId = $2
 ;
-
 -- Add menu item
-INSERT INTO Menu (itemId, restuarnatId, itemName, price, category, dailyLimit) VALUES ($1, $2, $3, $4, $5, $6);
-
+INSERT INTO Menu (itemId, restaurantId, itemName, price, category, dailyLimit) VALUES ($1, $2, $3, $4, $5, $6);
 -- Select promo
+
+
 
 /*Customer Functions*/
 -- Get all restaurants
-SELECT DISTINCT restuarnatId, name,  area, minSpendingAmt
+SELECT DISTINCT restaurantId, name,  area, minSpendingAmt
 FROM Restaurants
 ;
--- Get all resturants from area
-SELECT DISTINCT restuarnatId, name,  area, minSpendingAmt
+-- Get all restaurants from area
+SELECT DISTINCT restaurantId, name,  area, minSpendingAmt
 FROM Restaurants
 where area = $1
 ;
--- Get info for specific resturant
-SELECT DISTINCT restuarnatId, name,  area, minSpendingAmt
+-- Get info for specific restaurant
+SELECT DISTINCT restaurantId, name,  area, minSpendingAmt
 FROM Restaurants
 WHERE restaurantId = $1
 ;
--- See all menu items for selected resturant
+-- See all menu items for selected restaurant
 SELECT DISTINCT itemId, itemName, price, category, isAvailable, dailyLimit
 FROM Menus
 WHERE restaurantId = $1
@@ -93,18 +66,53 @@ SELECT DISTINCT itemId, itemName, price, category, isAvailable, dailyLimit
 FROM Menus
 WHERE restaurantId = $1 AND category = $2
 ;
--- Add menu items to order
-
 -- Make/ Add review
 INSERT INTO Reviews (reviewId, orderId, review, rating) VALUES ($1, $2, $3, $4);
-
+--See review posting
+SELECT DISTINCT Res.name as Restaurant, O.dateOfOrder as OrderDate, Rev.review as Review, Rev.rating as Rating
+FROM Restaurant Res JOIN Orders O ON (Res.restaurantId = O.restaurantId) JOIN Reviews Rev ON (O.orderId = Rev.orderId)
+ORDER BY Res.name
+;
+-- See past orders
+SELECT O.dateOfOrder as OrderDate, Res.name as Restaurant, M.itemName as ItemName, OD.quantity as Quantity
+FROM Orders O JOIN Restaurants Res ON (O.restaurantId = Res.restaurantId)
+JOIN OrderDetails OD ON (O.orderId = OD.orderId)
+JOIN Menus M ON (M.restaurantId = Res.restaurantId) AND (M.itemId = OD.itemID)
+WHERE O.customerId = $1
+ORDER BY O.dateOfOrder
+;
+-- Add menu items to order
+INSERT INTO OrderDetails (orderId, itemID, quantity, promotionId, orderCost, pointsObtained, pointsRedeemed) VALUES ($1, $2, $3, $4, $5, $6, $7);
 -- select price range
+SELECT R.name as RestaurantName, minSpendingAmt, itemName, price, isAvailable, minSpendingAmt
+FROM Menus M JOIN Restaurants R ON (M.restaurantId = R.restaurantId)
+WHERE price >= $1 AND price <=$2
 -- select payment method
+UPDATE Orders
+SET paymentMode = $1
+WHERE orderId = $2
+;
 -- enter card number (if payment by card)
+INSERT INTO CreditCards (customerId, cardNumber) VALUES ($1, $2)
 -- select recent order
+SELECT O.orderId, R.name as RestaurantName, M.itemName, OD.quantity
+FROM Orders O JOIN OrderDetails OD ON (O.orderId = OD.orderId)
+JOIN Restaurants R ON (O.restaurantId = R.restaurantId) JOIN Menus M ON (OD.itemId = M.itemId)
+WHERE O.customerId = $1
+AND (O.dateOfOrder, O.timeOfOrder) >= ALL (
+  SELECT dateOfOrder, timeOfOrder
+  FROM Orders O1
+  WHERE O1.orderId = O.orderId
+  AND O1.customerId = $1
+);
 -- select promo
---view review posting
---view past orders
+UPDATE OrderDetails
+SET promotionId = $1
+WHERE orderId = $2
+;
+
+
+
 
 
 /*Fds Manager Functions*/
